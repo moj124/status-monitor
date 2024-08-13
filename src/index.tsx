bd03e158b0e15c1da9efe8adf72247e773c96ba4
moj124 <michael.jowen.jones@gmail.com>
2024-08-13 23:28:27 +0100

    refactor index optimise for production

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,13 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+if (process.env.NODE_ENV === 'development') {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  root.render(<App />);
+}
